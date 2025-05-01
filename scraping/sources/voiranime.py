a79507b372b1686b2beeb346f8d241b93af2b4a4
FLo <florifavi@gmail.com>
2025-05-01 19:10:12 +0200

    ✨ [FIX] Correction de la pagination pour VoirAnime

--- scraping/sources/voiranime.py
+++ scraping/sources/voiranime.py
@@ -44,7 +44,7 @@ logger = logging.getLogger('voiranime_scraper')
 load_dotenv()
 
 # Configuration des constantes
-BASE_URL = "https://voiranime.com"
+BASE_URL = "https://v6.voiranime.com"
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
